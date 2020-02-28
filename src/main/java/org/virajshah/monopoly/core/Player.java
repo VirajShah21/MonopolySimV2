6e4e177e0cb459755f0a15f67dc1aea76bd656c1
Viraj Shah <shahvirajd@gmail.com>
2020-02-28 18:02:36 -0500

    Added new fields required for MonopolyGame

--- src/main/java/org/virajshah/monopoly/core/Player.java
+++ src/main/java/org/virajshah/monopoly/core/Player.java
@@ -1,21 +1,48 @@
 package org.virajshah.monopoly.core;
 
+import java.util.ArrayList;
+
+import org.virajshah.monopoly.core.beans.TurnHistoryBean;
+import org.virajshah.monopoly.tiles.PropertyTile;
+
 public class Player {
 	private String name;
 	private int balance;
+	private MonopolyGame game;
+	private int position;
+	private boolean prisoner;
+	private ArrayList<TurnHistoryBean> turnHistory;
+	private ArrayList<PropertyTile> properties;
 
 	public Player(String name) {
 		this.name = name;
 		this.balance = 1500;
+		position = 0;
+		turnHistory = new ArrayList<>();
+		properties = new ArrayList<>();
 	}
 
-	public void transaction(int amount, Player other) {
+	public void sendMoney(int amount, Player other) {
 		// TODO: Implement
 	}
 
 	public boolean transaction(int amount) {
 		balance += amount;
 		return true;
+	}
+
+	/**
+	 * @return the game
+	 */
+	public MonopolyGame getGame() {
+		return game;
+	}
+
+	/**
+	 * @param game the game to set
+	 */
+	public void setGame(MonopolyGame game) {
+		this.game = game;
 	}
 
 	/**
@@ -46,4 +73,52 @@ public class Player {
 		this.balance = balance;
 	}
 
+	/**
+	 * @return the turnHistory
+	 */
+	public ArrayList<TurnHistoryBean> getTurnHistory() {
+		return turnHistory;
+	}
+
+	/**
+	 * @param turnHistory the turnHistory to set
+	 */
+	public void setTurnHistory(ArrayList<TurnHistoryBean> turnHistory) {
+		this.turnHistory = turnHistory;
+	}
+
+	/**
+	 * @return the position
+	 */
+	public int getPosition() {
+		return position;
+	}
+
+	/**
+	 * @param position the position to set
+	 */
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	
+	/**
+	 * @return the prisoner
+	 */
+	public boolean isPrisoner() {
+		return prisoner;
+	}
+
+	/**
+	 * @param prisoner the prisoner to set
+	 */
+	public void setPrisoner(boolean prisoner) {
+		this.prisoner = prisoner;
+	}
+	
+	/**
+	 * @return the properties
+	 */
+	public ArrayList<PropertyTile> getProperties() {
+		return properties;
+	}
 }
